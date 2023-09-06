9c4048c4bced7f1ecd94a94fc806b848c5683732
ddemoro <derek.demoro@gmail.com>
2023-09-06 09:39:35 -0700

    Updating

--- functions/.eslintrc.js
+++ functions/.eslintrc.js
@@ -28,5 +28,7 @@ module.exports = {
     "quotes": ["error", "double"],
     "import/no-unresolved": 0,
     "indent": ["error", 2],
+    "max-len": ["error", 200],
+    "@typescript-eslint/no-explicit-any": "off",
   },
 };
--- functions/src/profiles.ts
+++ functions/src/profiles.ts
@@ -4,7 +4,7 @@ import {Profile} from "./types";
 // eslint-disable-next-line @typescript-eslint/no-var-requires
 const FieldValue = require("firebase-admin").firestore.FieldValue;
 
-// eslint-disable-next-line max-len
+
 exports.profileAdded = functions.firestore.document("profiles/{uid}").onCreate(async (snap, context) => {
   const profile = Object.assign({id: snap.id}, snap.data() as Profile);
   console.log(profile);
@@ -18,3 +18,7 @@ exports.profileAdded = functions.firestore.document("profiles/{uid}").onCreate(a
   return Promise.resolve();
 });
 
+exports.resize = functions.https.onRequest(async (req, res) => {
+  console.log("YES");
+  res.sendStatus(200);
+});
